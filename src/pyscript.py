7c8c8bfdd33534ca5ecc43ddde0ffdb41eba9d66
Mindphil <philipumeadi@icloud.com>
2025-07-18 11:57:04 -0400

    Add

---
+++ src/pyscript.py
@@ -0,0 +1,12 @@
+import os
+from datetime import datetime
+import pandas as pd
+import re
+from pypdf import PdfReader
+import datefinder
+from rapidfuzz import process
+from dateutil import parser as date_parser
+
+excel_path = input(r"Enter the path to the Excel file: ")
+owner_df = pd.read_excel(excel_path, sheet_name= input("Please enter the name of the spreadsheet"), header=1)
+print()
